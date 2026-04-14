65eb090100802f072d6a7c2b5636f926396ca364
Neko Ayaka <neko@ayaka.moe>
2026-04-15 05:35:09 +0800

    chore(deps): bump dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,47 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  unocss: false,
-  formatters: true,
-  rules: {
-    'antfu/import-dedupe': 'error',
-    'import/order': 'off',
-    'perfectionist/sort-imports': [
-      'error',
-      {
-        groups: [
-          'type-builtin',
-          'type-import',
-          'value-builtin',
-          'value-external',
-          'type-internal',
-          ['type-parent', 'type-sibling', 'type-index'],
-          'value-internal',
-          ['value-parent', 'value-sibling', 'value-index'],
-          ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
-          'side-effect',
-          'style',
-        ],
-        newlinesBetween: 'always',
-      },
-    ],
-    'style/padding-line-between-statements': 'error',
+export default antfu(
+  {
+    unocss: false,
+    formatters: true,
+    rules: {
+      'antfu/import-dedupe': 'error',
+      'import/order': 'off',
+      'style/padding-line-between-statements': 'error',
+    },
   },
-})
+  {
+    ignores: [
+      '**/*.md',
+    ],
+    rules: {
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          groups: [
+            'type-builtin',
+            'type-import',
+            'type-internal',
+            ['type-parent', 'type-sibling', 'type-index'],
+            'default-value-builtin',
+            'named-value-builtin',
+            'value-builtin',
+            'default-value-external',
+            'named-value-external',
+            'value-external',
+            'default-value-internal',
+            'named-value-internal',
+            'value-internal',
+            ['default-value-parent', 'default-value-sibling', 'default-value-index'],
+            ['named-value-parent', 'named-value-sibling', 'named-value-index'],
+            ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
+            ['value-parent', 'value-sibling', 'value-index'],
+            'side-effect',
+            'style',
+          ],
+          newlinesBetween: 1,
+        },
+      ],
+    },
+  },
+)
